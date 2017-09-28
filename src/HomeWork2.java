da7e4068642034a0ef68d54860fac090b5775016
makcyt01 <makcyt01@gmail.com>
2017-09-28 15:39:10 +0300

    додали ввід даних з консолі

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -11,6 +11,16 @@ public class HomeWork2 {
         //створюєм об'єкт Scanner
         Scanner scan = new Scanner(System.in);
 
-        //
+        //виводим в консоль запроси для юзера і ініціалізуємо змінні введеними даними
+        System.out.print("Ваше ім'я: ");
+        name = scan.nextLine();
+        System.out.print("Місто проживання: ");
+        city = scan.nextLine();
+        System.out.print("Ваш вік: ");
+        age = scan.nextInt();
+        System.out.print("Ваше хоббі: ");
+        hobby = scan.nextLine();
+
+        
     }
 }
