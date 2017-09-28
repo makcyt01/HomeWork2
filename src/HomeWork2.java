a506aa8cdc2f1d76bc945cfce8a6efb418fc2940
makcyt01 <makcyt01@gmail.com>
2017-09-28 15:22:15 +0300

    створення змінних і об'єкта Scanner

---
+++ src/HomeWork2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        //створюєм змінні
+        String name;
+        String city;
+        int age;
+        String hobby;
+
+        //створюєм об'єкт Scanner
+        Scanner scan = new Scanner(System.in);
+
+        //
+    }
+}
