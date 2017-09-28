48ce60fb5fcf6e4902dabeba035fd1444ac3c6d5
makcyt01 <makcyt01@gmail.com>
2017-09-28 16:33:54 +0300

    змінив Scanner на BufferedReader і дописав вивід в консоль

--- src/HomeWork2.java
+++ src/HomeWork2.java
@@ -1,26 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class HomeWork2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //створюєм змінні
         String name;
         String city;
         int age;
         String hobby;
 
-        //створюєм об'єкт Scanner
-        Scanner scan = new Scanner(System.in);
+        //створюєм об'єкт BufferedReader для зчитування даних з консолі
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
 
         //виводим в консоль запроси для юзера і ініціалізуємо змінні введеними даними
         System.out.print("Ваше ім'я: ");
-        name = scan.nextLine();
+        name = read.readLine();
         System.out.print("Місто проживання: ");
-        city = scan.nextLine();
+        city = read.readLine();
         System.out.print("Ваш вік: ");
-        age = scan.nextInt();
+        age = Integer.parseInt(read.readLine());
         System.out.print("Ваше хоббі: ");
-        hobby = scan.nextLine();
+        hobby = read.readLine();
 
-        
+        //виводим інформацію в консоль
+        System.out.println("Ім'я: " + name + "\n" +
+                            "Місто: " + city + "\n" +
+                            "Вік: " + age + "\n" +
+                            "Хоббі: " + hobby);
+        System.out.println("-----------------------");
+        System.out.println("Людина на ім'я " + name + " проживає в місті " + city + ".\n" +
+                            "Цій людині " + age + " років і її хоббі " + hobby + ".");
+        System.out.println("-----------------------");
+        System.out.println(name + " - ім'я\n" +
+                            city + " - місто\n" +
+                            age + " - вік\n" +
+                            hobby + " - хоббі");
     }
 }
